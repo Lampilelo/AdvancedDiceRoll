a568bfefd469585918532315320aab72dba75b75
Lampilelo <cubex7@gmail.com>
2017-06-13 12:23:49 +0200

    Added RollResult source and some tests.

--- src/Operations/RollResult.cpp
+++ src/Operations/RollResult.cpp
@@ -8,9 +8,27 @@ RollResult::RollResult()
 void RollResult::appendErrorLog(const std::string error)
 {
     _errorLog.push_back(error);
+    _hasErrors = true;
 }
 
 bool RollResult::hasErrors() const
 {
-    return !_hasErrors;
+    return _hasErrors;
+}
+
+const std::string& RollResult::getError(int index) const
+{
+    //TODO: We should check bounds.
+    return _errorLog[index];
+}
+
+const std::vector<std::string>& RollResult::getErrorLog() const
+{
+    return _errorLog;
+}
+
+void RollResult::append(const RollResult *item)
+{
+    for(auto& error : item->getErrorLog())
+	_errorLog.push_back(error);
 }
--- test/RollResultTest.cpp
+++ test/RollResultTest.cpp
@@ -6,3 +6,40 @@ TEST(RollResultTest, InitWithEmptyErrorLog)
     RollResult rr;
     EXPECT_FALSE(rr.hasErrors());
 }
+
+TEST(RollResultTest, CanAppendErrorLog)
+{
+    RollResult rr;
+    for (int i = 0; i < 10; i++)
+	rr.appendErrorLog("Error " + std::to_string(i));
+    
+    for (int i = 0; i < 10; i++)
+	EXPECT_EQ("Error " + std::to_string(i), rr.getError(i));
+}
+
+TEST(RollResultTest, HasErrorsAfterAppendingErrorLog)
+{
+    RollResult rr;
+    rr.appendErrorLog("Error");
+
+    EXPECT_TRUE(rr.hasErrors());
+}
+
+TEST(RollResultTest, GetErrorLogIsWorking)
+{
+    RollResult rr;
+    EXPECT_TRUE(rr.getErrorLog().empty());
+}
+
+TEST(RollResultTest, AppendsErrorsWithAnotherRollResult)
+{
+    RollResult rr_one;
+    rr_one.appendErrorLog("Error 0");
+    RollResult rr_two;
+    rr_two.appendErrorLog("Error 1");
+    rr_two.appendErrorLog("Error 2");
+    rr_one.append(&rr_two);
+
+    for (int i = 0; i < 3; i++)
+	EXPECT_EQ("Error " + std::to_string(i), rr_one.getError(i));
+}
