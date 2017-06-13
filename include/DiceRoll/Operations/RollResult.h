be198daa86b238666fa3c97b42498312dc22505c
Lampilelo <cubex7@gmail.com>
2017-06-13 12:20:04 +0200

    Added include guard (oops), changed append function and add new getErrorLog.

--- include/DiceRoll/Operations/RollResult.h
+++ include/DiceRoll/Operations/RollResult.h
@@ -1,5 +1,9 @@
+#ifndef ROLLRESULT_H
+#define ROLLRESULT_H
+
 #include <vector>
 #include <string>
+#include <memory>
 
 class RollResult
 {
@@ -11,8 +15,12 @@ public:
     RollResult();
 
     const std::string& getError(int index) const;
+    const std::vector<std::string>& getErrorLog() const;
     void appendErrorLog(std::string error);
     bool hasErrors() const;
-    void append(const RollResult& item);
+    void append(const RollResult* item);
     /* void appendRawResult(std::string ) */
 };
+
+
+#endif // ROLLRESULT_H
