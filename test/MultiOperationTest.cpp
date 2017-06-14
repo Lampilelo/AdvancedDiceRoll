5f181e3cc3cbd02454caf340f2e1c2640ab58a27
Lampilelo <cubex7@gmail.com>
2017-06-14 13:48:14 +0200

    Added temporary test for decorator pattern

---
+++ test/MultiOperationTest.cpp
@@ -0,0 +1,20 @@
+#include <memory>
+
+#include "gtest/gtest.h"
+
+#include "DiceRoll/Operations/RandomOperation.hpp"
+#include "DiceRoll/Operations/RepeatOperation.h"
+
+TEST(MultiOperationTest, PassingRollResultGenericEvaluate)
+{
+    RandomOperation rand(6);
+    rand.changeSeed(2134);
+    RepeatOperation repeat(&rand, 4);
+
+    std::unique_ptr<RollResult> result = repeat.evaluate();
+    EXPECT_EQ("RandomOperation: 1", result->getErrorLog()[0]);
+    EXPECT_EQ("RepeatOperation", result->getErrorLog()[1]);
+
+    // Fail because it's a temporary test.
+    FAIL();
+}
