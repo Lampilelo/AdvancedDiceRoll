ab70b069b728c3d79ee04dbd918e01f34b9f9cf3
Lampilelo <cubex7@gmail.com>
2017-06-16 13:25:58 +0200

    Added rvalue setters and one new method

--- include/DiceRoll/Operations/RollResult.h
+++ include/DiceRoll/Operations/RollResult.h
@@ -27,15 +27,24 @@ public:
 
     inline const std::string& getOperationLog() const
     { return _operationLog; }
-
+    
     inline void setOperationLog(const std::string& newOpLog)
     { _operationLog = newOpLog; }
+
+    inline void setOperationLog(std::string&& newOpLog)
+    { _operationLog = std::move(newOpLog); }
 
     inline const std::vector<int>& getLastResult() const
     { return _lastResult; }
 
     inline void setLastResult(std::vector<int> &newResult)
     { _lastResult = newResult; }
+
+    inline void setLastResult(std::vector<int>&& newResult)
+    { _lastResult = std::move(newResult); }
+
+    inline void appendLastResult(int number)
+    { _lastResult.push_back(number); }
 };
 
 
