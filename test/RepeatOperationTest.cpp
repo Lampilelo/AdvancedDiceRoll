c9948ef3d6197090e83e3c6037f15245f2ef4373
Lampilelo <cubex7@gmail.com>
2017-10-04 22:35:28 +0200

    Fixed MockOperation.

--- test/RepeatOperationTest.cpp
+++ test/RepeatOperationTest.cpp
@@ -5,18 +5,20 @@
 
 class MockOperation : public IOperation
 {
-    static int count;
-
     std::unique_ptr<RollResult> execute()
 	{ return std::make_unique<RollResult>(); }
 
 public:
+    static int count;
+
     MockOperation(): IOperation(nullptr){};
-    
+
+    // For each evaluate() static int count is incrementing.
     std::unique_ptr<RollResult> evaluate()
 	{
 	    auto result = std::make_unique<RollResult>();
-	    result->setOperationLog(std::to_string(count));
+	    result->setOperationLog(std::to_string(++count));
+	    result->setLastResult(count);
 	    return result;
 	}
 };
