5e7c1b11aed42a31dfd29f6139c45eec1df8a3b0
Lampilelo <cubex7@gmail.com>
2018-03-22 11:00:25 +0100

    Removed .dir-locals.el.

--- .dir-locals.el
+++
@@ -1,6 +0,0 @@
-((nil . ((eval . (set (make-local-variable 'cmake-ide-build-dir)
-		      (concat
-		       (file-name-directory
-		       (let ((d (dir-locals-find-file ".")))
-			 (if (stringp d) d (car d))))
-		       "build"))))))
