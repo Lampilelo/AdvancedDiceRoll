428fe497c88f35481bf406bce0347adb54c78a20
Lampilelo <cubex7@gmail.com>
2018-07-25 12:44:50 +0200

    Check for ComplexOperation constructor to not take nullptr as an arg

--- src/Operations/ComplexOperation.cpp
+++ src/Operations/ComplexOperation.cpp
@@ -2,5 +2,9 @@
 
 namespace DiceRoll {
 ComplexOperation::ComplexOperation(IOperation* componentOp) :
-    componentOp_(componentOp) { }
+    componentOp_(componentOp) {
+  if (nullptr == componentOp)
+    throw std::invalid_argument(
+        "In ComplexOperation constructor: passed nullptr as componentOp.");
+}
 }
